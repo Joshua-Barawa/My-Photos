948f946cfa62ba5708af050927aceea921032cfd
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-04 17:21:25 +0300

    added database configurations

--- myphotos/settings.py
+++ myphotos/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myphotos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fakebookdb',
+        'USER': 'fakebook',
+        'PASSWORD': 'letmein',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
