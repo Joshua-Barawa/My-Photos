45c9c32867ffc53bec8d6e233184269b182b92df
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-06 12:01:33 +0300

    created registration form

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,  login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_user(request):
@@ -13,9 +14,32 @@ def login_user(request):
             return redirect('home-page')
         else:
             messages.success(request, "Invalid username or password")
-            return redirect('login_user')
-
+            return redirect('login-user')
     else:
         return render(request, 'members/login.html', {})
 
     return render(request, 'members/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "successfully logged out")
+    return redirect('login-user')
+
+
+def register_user(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            login(request. user)
+            messages.success(request, "Registration successful")
+            return redirect('home-page')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'members/register.html', {'form': form})
--- useraccount/models.py
+++ useraccount/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     profile = models.ImageField(blank=True, null=True)
-    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     bio = models.TextField(max_length=200)
 
     def __str__(self):
@@ -17,7 +17,6 @@ class Image(models.Model):
     caption = models.TextField(max_length=200)
     like = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
-    posted_on = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
