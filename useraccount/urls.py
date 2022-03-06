7e6726688abb5ca9566d86733ac20e32888f0eec
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-03-06 15:03:43 +0300

    added details page

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path("", home, name='home-page')
+    path("", home, name='home-page'),
+    path('get-image/<int:id>/', get_image_by_id, name="get_image"),
 
 ]
--- useraccount/views.py
+++ useraccount/views.py
@@ -7,3 +7,10 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     posts = Image.objects.all()
     return render(request, 'useraccount/index.html', {'posts': posts})
+
+
+@login_required(login_url='/members/login-user')
+def get_image_by_id(request, id):
+    image = Image.objects.get(pk=id)
+    return render(request, "useraccount/post.html", {"image": image})
+
